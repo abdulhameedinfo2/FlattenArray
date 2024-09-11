3104cd326442f2e73b5892e5807a8a48b27813c5
abdulhameed@escrowtab.com <167234810+abdulhameedinfoearlystage@users.noreply.github.com>
2024-09-11 23:01:31 +0500

    Add function to flatten the array.
    The function flattens the array and out the result.

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿
+var arr = new object[] { 1, new object[] { 2, new object[] { 3, 4 } }, 5 };
+var result = new FlattenArray().Flatten(arr);
+Console.WriteLine(string.Join(", ", result));
+public class FlattenArray
+{
+    public int[] Flatten(object[] arr)
+    {
+        var flattenArray = new List<int>();
+        FlattenRecursive(arr, flattenArray);
+        return flattenArray.ToArray();
+    }
+
+    private void FlattenRecursive(object[] arr, List<int> flattenArray)
+    {
+        foreach (object o in arr)
+        {
+            if (o is int)
+            {
+                flattenArray.Add((int)o);
+            }
+            else if (o is object[])
+            {
+                FlattenRecursive((object[])o, flattenArray);
+            }
+        }
+    }
+}
